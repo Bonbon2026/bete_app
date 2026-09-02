0ceeed2d74240d7d4d4871f2639829fcd9a675b0
Bonbon2026 <moybonmelaku2020@gmail.com>
2026-09-02 15:30:19 +0300

    Wire up Supabase connection

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,5 @@
+class SupabaseConfig {
+  static const String url = 'https://ycdschfmcbjrdlvdzxqi.supabase.co';
+  static const String anonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljZHNjaGZtY2JqcmRsdmR6eHFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNDYxNTIsImV4cCI6MjEwMzkyMjE1Mn0.xgqDhbMkv0xwgml23nLjnIk5UIlG_Uq2cuwurK7Xilg';
+}
